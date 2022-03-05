d88afe2feaaf188ebc3d40b9703b2dbaecadf28f
JohnScreamer <bogaturscreamer@gmail.com>
2022-03-05 13:42:48 +0200

    1

--- src/modules/second_page/second_page.js
+++
@@ -1,5 +0,0 @@
-
-const text= document.querySelector('.text1').addEventListener('click',(e)=>{
-   document.querySelector('.text1').style.color='red';
-})
-export {text};
--- src/modules/thrid_page/thrid_page.js
+++
@@ -1,7 +0,0 @@
-let zxc = document.querySelector('.text1').addEventListener('mouseover',(e)=>{
-   document.querySelector('.text1').style.color= 'yellow';
-   console.log(e);
-})
-   
-
-export{zxc};
